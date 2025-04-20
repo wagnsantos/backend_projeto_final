8719a9c508a33c24aa55e62734fa6df9e3822931
wagnsantos <wagnsantos02@gmail.com>
2025-04-20 01:06:47 -0300

    chore: foi feita manutença no código

--- src/server.ts
+++ src/server.ts
@@ -5,19 +5,17 @@ import cors from '@fastify/cors'
 const app = Fastify({ logger: true })
 
 app.setErrorHandler((error, request, reply) => {
-  reply.code(400).send({ message: error.message }) // Corrigi "massege" → "message"
+  reply.code(400).send({ message: error.message })
 })
 
-const start = async () => {
+const setup = async () => {
   await app.register(cors)
   await app.register(routes)
-
-  try {
-    await app.listen({ port: 3333, host: '0.0.0.0' }) // <- necessário para o Render
-  } catch (err) {
-    app.log.error(err)
-    process.exit(1)
-  }
+  await app.ready()
 }
 
-start()
+setup()
+
+export default async (req: any, res: any) => {
+  app.server.emit('request', req, res)
+}
